19c6887f46ae42e980602b3db955a082124fd0c9
GABRIEL LEITE <leite06@hotmail.com>
2022-04-25 20:58:09 -0300

    .gitignore is now working

--- palindrome-number/target/debug/deps/palindrome_number.d
+++
@@ -1,5 +0,0 @@
-C:\git\leetcode\palindrome-number\target\debug\deps\palindrome_number.exe: src\main.rs
-
-C:\git\leetcode\palindrome-number\target\debug\deps\palindrome_number.d: src\main.rs
-
-src\main.rs:
--- palindrome-number/target/debug/palindrome-number.d
+++
@@ -1,1 +0,0 @@
-C:\git\leetcode\palindrome-number\target\debug\palindrome-number.exe: C:\git\leetcode\palindrome-number\src\main.rs
